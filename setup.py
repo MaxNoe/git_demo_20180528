43154b12380f9d815f7eb7a2a416e7febb69c710
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-05-28 17:45:36 +0200

    Add fib module

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='fib',
+    packages=find_packages(),
+    author='Maximilian Nöthe',
+)
---
+++ tests/test_fib.py
@@ -0,0 +1,10 @@
+def test_fib0():
+    from fib import fib
+
+    assert fib(0) == 1
+
+
+def test_fib5():
+    from fib import fib
+
+    assert fib(5) == 8
